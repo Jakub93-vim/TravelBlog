7dc9e1f958c8e167352a1188aa94c3d90284490d
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-04-11 20:17:31 +0200

    flask app

--- TravelBlog.py
+++ TravelBlog.py
@@ -5,4 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return '<h1>Hello, Home!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
+
+
+if __name__ == '__main__':
+    app.run (debug = True)
