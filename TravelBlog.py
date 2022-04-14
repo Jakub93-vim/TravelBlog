68fae469d8e6e984b0cbfa808eefa78036c1aec7
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-04-14 21:01:12 +0200

    templates

--- TravelBlog.py
+++ TravelBlog.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, Home!</h1>'
+@app.route('/home')
+def home():
+    return render_template ('home.html')
 
 @app.route('/about')
 def about():
-    return '<h1>About page</h1>'
+    return render_template ('about.html')
 
 
 if __name__ == '__main__':
