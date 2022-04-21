f162050799bbc8e63f3f37f703bff0b961288fbd
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-04-21 21:49:05 +0200

    templates login and register

--- TravelBlog.py
+++ TravelBlog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ju5df4f5669vfdvvd5'
 
 posts = [
 
@@ -29,6 +31,17 @@ def home():
 def about():
     return render_template ('about.html')
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', 
+            title = 'Register', form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+            title = 'Login', form = form)
 
 if __name__ == '__main__':
     app.run (debug = True)
