ad4575613e1c241b7f4c4137ce6c2e5ce76baf44
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-05-01 22:27:39 +0200

    register works

--- forms.py
+++ forms.py
@@ -5,13 +5,13 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
 
 	username = StringField ('Username', 
-			validators=[DataRequired(), Length(min=2, max=20)]
+			validators=[DataRequired(), Length(min=2, max=20)])
 
 	email = StringField('Email', 
 			validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()]
+	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password',
-			validators=[DataRequired(), EqualTo('password')]
+			validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
 
@@ -19,6 +19,6 @@ class LoginForm(FlaskForm):
 
 	email = StringField('Email', 
 			validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()]
+	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
