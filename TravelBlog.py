41cd78c6d7937828cb210e850fcbe3804418a6e0
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-05-23 20:45:30 +0200

    database

--- TravelBlog.py
+++ TravelBlog.py
@@ -21,7 +21,7 @@ posts = [
     }
 ]
 
-
+# home webpage
 @app.route('/')
 @app.route('/home')
 def home():
@@ -30,6 +30,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template ('about.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
