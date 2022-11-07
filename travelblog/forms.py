f2f73d6dc5f57b88625af6f5fd58e2b7c46736ad
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-11-07 08:06:45 +0100

    fields check

--- travelblog/forms.py
+++ travelblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from travelblog.models import User
 
 class RegistrationForm(FlaskForm):
 
@@ -14,9 +15,15 @@ class RegistrationForm(FlaskForm):
 			validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
-	def validate_field(seld, field):
-		if True:
-			raise ValidationError('Validation Message')
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('That username is taken. Please choose different one.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('That email is taken. Please choose different one.')
 
 class LoginForm(FlaskForm):
 
