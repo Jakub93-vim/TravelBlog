ffffbf6276485f617252cc5b392dfb3289326a60
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-11-08 08:16:59 +0100

    login redirect

--- travelblog/routes.py
+++ travelblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from travelblog import app, db, bcrypt
 from travelblog.forms import RegistrationForm, LoginForm
 from travelblog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user
 
 posts = [
 
@@ -35,6 +35,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -48,11 +50,15 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.chech_password_hash(user.password, form.password.data):
-
-        flash('Login Unsuccessful. Please check username and password', 'danger')
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', 
             title = 'Login', form = form)
