dd312569a696d3e50189ce931d84c3ce1234d969
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-11-22 18:46:19 +0100

    change password

--- travelblog/routes.py
+++ travelblog/routes.py
@@ -149,3 +149,23 @@ def user_posts(username):
         .order_by(Post.date_posted.desc())\
         .paginate(page=page, per_page=3)
     return render_template ('user_posts.html', posts = posts, user=user)
+
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    return render_template('reset_request.html', title = 'Reset Password', form=form)
+    
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flask('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title = 'Reset Password', form=form)
